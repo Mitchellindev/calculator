68c9d5781da8fc649f64c25acbb98a68949dd4b0
Aghware Mitchellin <96637672+Mitchellindev@users.noreply.github.com>
2022-04-29 07:36:58 +0100

    Update script.js

--- calculator/script.js
+++ calculator/script.js
@@ -42,19 +42,3 @@ buttons.map( buttons =>{
 
 
 
-// function display(num){
-//     output.value += num;
-//     function calculate(){
-//         try{
-//             output.value = eval(output.value);
-//         }
-//         catch(err){
-//             alert("invalid expression")
-//         }
-//     }
-
-//     function del(){
-//         output.value = output.value.slice(0,-1);
-//     }
-// }
-
